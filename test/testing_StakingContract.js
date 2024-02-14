b2bf2484e1d808809992029ba893fb8e377e3669
Philijon <Jonaskullin@web.de>
2024-02-14 12:20:32 +0100

    commenting stakingcontract testfile

--- test/testing_StakingContract.js
+++ test/testing_StakingContract.js
@@ -1,32 +1,37 @@
+// This file will use ganache / truffle / mocha / chai to perform unit tests on the staking contract
+// with the help of the testerContract, which is written only for that case
+
+// import necessary artifacts 
 const stakingContract = artifacts.require("stakingContract");
 
 const ERC721Enumerable = artifacts.require("ERC721Enumerable");
 
-const testerContract = artifacts.require("testerContract");
 
 
-// let address1 = "0x64b7FFC15Fa8EC85d2b56835516CcA3895505067";
-// let address2 = "0xDC2E35bdf7372eDdd82C51AC77d0C4FdF8081e5b";
-// let address3 = "0xfD9faAd8af18ff0ebA111B8897c069d2f9479180";
-// let address4 = "0xC8E5A47f2b716616883598365d194D1623c48B5b";
-// let address5 = "0x932e5B1C4649245362f4E7c7Cc0d3cA110f95B5D";
-// let address6 = "0x9696E20A1D0196C725C724120f3e5A5f023b8260";
-// let address7 = "0x0a10861679DAeEa0b5FcfA8427f24eC914284d10";
-// let address8 = "0x74De322E95c23ACb62307B8c75f29da19F345214";
-// let address9 = "0xb4A02864A42aEaA9C287259b18555306295B7faF";
-// let address10 = "0xeC0E3c00028849ECCeAD4855581ccAF257CA7e0F";
+// adjust these addresses to the by ganache provided ones
+
+let address1 = "0x64b7FFC15Fa8EC85d2b56835516CcA3895505067";
+let address2 = "0xDC2E35bdf7372eDdd82C51AC77d0C4FdF8081e5b";
+let address3 = "0xfD9faAd8af18ff0ebA111B8897c069d2f9479180";
+let address4 = "0xC8E5A47f2b716616883598365d194D1623c48B5b";
+let address5 = "0x932e5B1C4649245362f4E7c7Cc0d3cA110f95B5D";
+let address6 = "0x9696E20A1D0196C725C724120f3e5A5f023b8260";
+let address7 = "0x0a10861679DAeEa0b5FcfA8427f24eC914284d10";
+let address8 = "0x74De322E95c23ACb62307B8c75f29da19F345214";
+let address9 = "0xb4A02864A42aEaA9C287259b18555306295B7faF";
+let address10 = "0xeC0E3c00028849ECCeAD4855581ccAF257CA7e0F";
 
 
-let address1 = "0x5ac5F3361D782DB973c9c65dF716D38307a755e2";
-let address2 = "0x58fBFbB09aA6f5f187dfCe903F9C8AB8AFbEC030";
-let address3 = "0x620F84B4a2d90c1c7fe9Ed6CC01F01427eE6a294";
-let address4 = "0xfa00fB886DaaE3230FEcE4bCC4C24008F6438fD0";
-let address5 = "0x89D23CFA2A3123F489DC896f62C5130CfdF504Fe";
-let address6 = "0x453DF75F581e621446b8c110819fA053C6A656d2";
-let address7 = "0x525Dc7C17655Fd080969dD040cb6B79764FF3403";
-let address8 = "0xDE6547B6fA7a5849B0Bd698393C154191211E275";
-let address9 = "0xEBb061067C47605EaCDA624BF2E6c48010937E48";
-let address10 = "0x29f33A3D42Fa43F6563aa98875b95789a6086D4d";
+// let address1 = "0x5ac5F3361D782DB973c9c65dF716D38307a755e2";
+// let address2 = "0x58fBFbB09aA6f5f187dfCe903F9C8AB8AFbEC030";
+// let address3 = "0x620F84B4a2d90c1c7fe9Ed6CC01F01427eE6a294";
+// let address4 = "0xfa00fB886DaaE3230FEcE4bCC4C24008F6438fD0";
+// let address5 = "0x89D23CFA2A3123F489DC896f62C5130CfdF504Fe";
+// let address6 = "0x453DF75F581e621446b8c110819fA053C6A656d2";
+// let address7 = "0x525Dc7C17655Fd080969dD040cb6B79764FF3403";
+// let address8 = "0xDE6547B6fA7a5849B0Bd698393C154191211E275";
+// let address9 = "0xEBb061067C47605EaCDA624BF2E6c48010937E48";
+// let address10 = "0x29f33A3D42Fa43F6563aa98875b95789a6086D4d";
 
 
 contract("Stakercontract",async()=>{
@@ -37,68 +42,106 @@ contract("Stakercontract",async()=>{
 
     before(async()=>{
 
+
+        // initializing two variables with instances of the deployed ERC721ENumerable contract,
+        // and the deployed stakingcontract with new parameters:
+        // param 1 : address of the deployed NFT contract
+        // param 2 : string which will be passed to the constructor as the name of the token paid as reward
+        // param 3 : string which will be passed to the constructor as the symbol of the token paid as reward
+        // param 4 : Number which will be passed to the constructor as the amount of tokens paid per second per NFT staked
         NFT = await ERC721Enumerable.deployed();
-        // Staker = await stakingContract.deployed();
         StakerContract = await stakingContract.new(NFT.address,"RewardToken","RWT",5000);
-        // console.log(NFT.address,Staker.address,StakerContract.address)
     });
 
-        
+
+    // testing the staker contract got deployed by checking its address
     it("should return the address of the deployed staking contract",async()=>{
-        assert(StakerContract.address != "");
+
+        // asserting the staker contract address returns a string of length = 42, 40 characters + 0x prefix
+        assert.lengthOf(StakerContract.address,42,"Stakercontract did appearently not deploy porperly");
     });
 
+
+    // testing the implementation of onERC721Received, which should return the bytes4 value 0x150b7a02,
+    // which represents the onERC721Received.selector
     it("will test the implementation of the erc721 onERC721Received functionality of the contract, which should return the onERC721Received selector",async()=>{
-        // selector = 0x150b7a02
+        // 0x150b7a02
         let result;
         try{
+
+            // calling the onERC721Received() function of staking contract, parameters representing the transferFrom() parameters
             result = await StakerContract.onERC721Received(address1,address2,1,"0x00");
-            // console.log(result)
         }catch(error){
             console.log(error);
         }
-        assert(result = 0x150b7a02)
+
+        // asserting the onERC721Received actually returned the correct value
+        assert.equal(result,0x150b7a02,"Stakercontract.onERC721Received did not return the correct bytes4 value")
     });
 
+
+    // approving the stakingcontract to transfer NFT tokenId 1 from address1 after minting it,
+    // which is necessary to call the staking function of the staker contract later
     it("should give the staking contract approval to transfer nft 1 from address1",async()=>{
         
-        //mint NFT1
+        // minting (restricted owner access) NFT tokenId 1 by address1, which is currently the owner of the NFT contract,
+        // thus msgSender specification unnecessary
+        // param 1 : address receiving the minted NFT
+        // param 2 : number tokenId to be minted
         try{
-            await NFT.mint(address1,1,{from: address1})
+            await NFT.mint(address1,1)
         }catch(error){
             console.log(error)
         }
 
 
+        // checking the new owner of the freshly minted NFT tokenId 1 using the getter function ownerOf()
         let owner = await NFT.ownerOf(1);
-        assert(owner = address1);
+
+        // asserting address1 is actually the owner of the NFT tokenId 1
+        assert.equal(owner,address1,"NFT tokenId 1 has not been succesfully minted to address1");
         
         try{
-            await NFT.approve(StakerContract.address,1,{from: address1});
+
+            // approving the stakercontract for NFT tokenId1, currently held by address1, msgSender specification unnecessary
+            await NFT.approve(StakerContract.address,1);
         }catch(error){
             console.log(error);
         }
 
+        // using the getter function getApproved() to get the approved address for NFT tokenId 1,
+        // which should now return the address of the staker contract
         let approved = await NFT.getApproved(1);
-        assert(approved==StakerContract.address)
 
-        
-        
+        // asserting the returned address is actually the address of the stakercontract
+        assert.equal(approved,StakerContract.address,"getApproved(1) did not return the address of the stakercontract") 
     });
 
+
+    // staking the NFT tokenId 1  and asserting it is then owned by the stakingcontract
     it("should stake the 1st nft, then check it is actually owned by the staking contract",async()=>{
+        
+        // making sure address1 has yet no NFTs staked by using the getterfunction stakedAmount()
         let stakedAmount = await StakerContract.stakedAmount(address1);
-        assert(stakedAmount==0);
+
+        // asserting the stakedAmount of address1 is currently 0
+        assert.equal(stakedAmount,0,"stakedAmount of address1 is appearently not 0");
 
         try{
+
+            // staking the NFT tokenId 1, currently held by address1,
+            // which also has to be the msgSender (reverting if transaction is initiaized by an approed spender),
+            // otherwise triggering the require statement "Only Owner of NFT can stake" of the stake() function
             await StakerContract.stake(1)
         }catch(error){
             console.log(error)
         }
 
-
+        // once again using the getter function stakedAmount()
+        stakedAmount = await NFT.stakedAmount(address1)
         let owner = await NFT.ownerOf(1);
-        assert(owner == StakerContract.address);
+        assert.equal(owner,StakerContract.address,"ownerOf(1) did not return the address of the stakercontract");
+        assert.equal(stakedAmount,1,"stakedAmount(address1) did not return 1")
     });
 
     
